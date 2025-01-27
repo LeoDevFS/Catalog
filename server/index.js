60ef3178d9b8083ba1fb4429e45e41887d881cb8
Leo <leonelsicaro@gmail.com>
2025-01-27 17:05:32 +0100

    client test and cors

--- server/index.js
+++ server/index.js
@@ -3,7 +3,10 @@ const app = express()
 const PORT = 3000
 const dbConnection = require('./config/config.js')
 const productRoutes = require('./routes/routes.js')
+const cors = require ("cors")
 
+
+app.use(cors())
 app.listen(PORT,()=>{
     console.log(`express is working on http://localhost:${PORT}`)
 })
