01e68f18bb6019d5813adb9a1bb8a1d47d8510dc
Leo <leonelsicaro@gmail.com>
2025-02-04 21:12:28 +0100

    modularize create

---
+++ client/js/createProduct.js
@@ -0,0 +1,30 @@
+document.getElementById('createForm').onsubmit = function(event) {
+    event.preventDefault(); 
+    const brand = document.getElementById('marca').value;
+    const sae = document.getElementById('sae').value;
+    createProduct(brand,sae);
+  };
+  console.log(`hola`)
+  function createProduct(marca, sae) {
+    fetch("http://localhost:3000/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        brand: marca,
+        sae: sae,
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log(sae);
+          window.alert("Product created successfully");
+        } else {
+          console.error("Error creating the product");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
