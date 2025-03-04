2fcdb55ef64e06416ab9eed8de44c225d2174a41
Leo <leonelsicaro@gmail.com>
2025-03-04 21:47:36 +0100

    moreTimeIInConfig

--- server/config/config.js
+++ server/config/config.js
@@ -4,7 +4,11 @@ require('dotenv').config()
 
 const dbConnection = async()=>{
     try {
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect(process.env.MONGO_URI, {
+       
+          
+            serverSelectionTimeoutMS: 30000 // Aumenta el tiempo de espera a 30 segundos
+          })
         console.log("mongo is working")
     } catch (error) {
         console.log(error)
