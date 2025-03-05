06ddfa99e75ac565795c29e2d229c53605607650
Leo <leonelsicaro@gmail.com>
2025-03-05 21:45:38 +0100

    productAplazed

--- client/catalog/src/App.jsx
+++ client/catalog/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import {BrowserRouter as Router,Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
-import ProductDetails from './components/ProductDetails'
+
 
 function App() {
 
@@ -11,7 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/dashboard' element={<Dashboard/>}/>
-          <Route path='/id/:_id' element={<ProductDetails/>}/>
+
         </Routes>
       </Router>
   )
